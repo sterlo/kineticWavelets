a334253b7cb128d4edae3b0dca0d1fdeade61d21
James Boocock <sfk2001@gmail.com>
2013-11-27 15:56:41 +1300

    fixed grepSeq.R added camel case variable names

--- R/grepSeq.R
+++ R/grepSeq.R
@@ -1,16 +1,16 @@
 <<<<<<< HEAD
 grepSeq <- function(seqz,patternz,returnCounts=T){
     seqGrep <- gregexpr(patternz,seqz)
-    wavePos=list()
+    grepPos=list()
     if( seqGrep[[1]][1]!=-1){
         for(i in 1:length(seqGrep[[1]])) {
-            wavePos[[i]] <- c(as.numeric(seqGrep[[1]][i]):c(-1+as.numeric(seqGrep[[1]][i])+as.numeric(attr(seqGrep[[1]], "match.length")[[i]])))
+            grepPos[[i]] <- c(as.numeric(seqGrep[[1]][i]):c(-1+as.numeric(seqGrep[[1]][i])+as.numeric(attr(seqGrep[[1]], "match.length")[[i]])))
     }
 
-    waveSeq <- c(rep(0,nchar(seqz)))
-    waveSeq[unlist(wavePos)] <- 1
+    grepSeq <- c(rep(0,nchar(seqz)))
+    grepSeq[unlist(grepPos)] <- 1
     if(returnCounts==T){cat("\n",length(seqGrep[[1]]),"regions found \n")}
-    return(waveSeq)
+    return(grepSeq)
     }
     if( seqGrep[[1]][1]==-1 ) {cat("\npattern not found \n")} 
 }
